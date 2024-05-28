39a8f1b5d62ca5edcf05556d538caab6dae94ffd
Jujomnova <147190309+Jujomnova@users.noreply.github.com>
2024-05-27 19:10:49 -0500

    Delete business directory

--- business/fachade/CrearTareaFachade.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uco.fink.business.fachade;
-
-import co.edu.uco.fink.dto.trabajos.TareaFincaDTO;
-
-public interface CrearTareaFachade {
-    void ejecutar(TareaFincaDTO Tarea);
-}
--- business/usecase/CrearTarea.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uco.fink.business.usecase;
-
-import co.edu.uco.fink.business.domain.TareaFincaDomain;
-
-public interface CrearTarea {
-    void ejecutar(TareaFincaDomain tarea);
-}
